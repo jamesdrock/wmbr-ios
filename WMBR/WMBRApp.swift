4a6fbf699cd0d9ae77b384b161a74750d4b55537
James D Rock <jdrock@mit.edu>
2024-05-31 22:14:23 +0300

    Initial Commit

---
+++ WMBR/WMBRApp.swift
@@ -0,0 +1,17 @@
+//
+//  WMBRApp.swift
+//  WMBR
+//
+//  Created by James D Rock on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WMBRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
